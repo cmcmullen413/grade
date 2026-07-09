a20343c9887546eb2265b750123063b34a36e68b
Caleb McMullen <cmcmullen@gmail.com>
2026-07-08 21:22:03 -0400

    Add Basic Program Functionallity and Begin Start
    Began working on program, stubbing out subcommands 'start' and 'submit'.
    Also began work on the 'start' subcommand. Currently just creates the
    specified directory if it does not exist as well as do some error
    handling for cases where existing paths are files not directories like
    they should be.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,100 @@
+use std::{env, fs};
+use std::path::{PathBuf};
+use clap::{command, Arg, ArgMatches, Command, builder::ValueParser};
+
 fn main() {
-    println!("Hello, world!");
+    // Define and get the command line arguments and commands
+    let matches = command!()
+        // Help message about the program itself
+        .about("TODO Description about the program")
+        // Prints the help message by default if no arguments are supplied
+        .arg_required_else_help(true)
+        // Command to initialize the grade book
+        // By default, it initializes to the current directory
+        .subcommand(
+            Command::new("start")
+                // Git name for this command
+                .alias("init")
+                // Description of this command
+                .about("Starts an empty grade book or restarts an old one")
+                // Optional Positional arg
+                // If provided, the new grade book is started in the specified subdirectory
+                // If it does not already exist, a new directory is created
+                .arg(
+                    Arg::new("dir")
+                        .help("(Optional) The directory to initialize the grade book in")
+                        // Specify that the passed value should be a path
+                        .value_parser(ValueParser::path_buf())
+                        .required(false)
+                )
+        )
+        // Command to create a new submit with the current changes
+        .subcommand(
+            Command::new("submit")
+                // Git name for this command
+                .alias("commit")
+                // Arg for passing a message directly instead of opening a text editor
+                .arg(
+                    Arg::new("message")
+                        .short('m')
+                        .long("message")
+                )
+        )
+        .get_matches();
+
+    // Depending on which subcommand was used, call the correct function with its arguments
+    match matches.subcommand() {
+        Some(("start", args)) => handle_start(args),
+        Some(("submit", args)) => handle_submit(args),
+        _ => ()
+    }
+}
+
+
+/// Handles the user calling the start command
+fn handle_start(args: &ArgMatches) {
+    // Get the directory or set it to the default (current working dir) if there is not
+    let dir = match args.get_one::<PathBuf>("dir") {
+        Some(dir) => dir.to_owned(),
+        None => env::current_dir().unwrap()
+    };
+
+    // First, check if the directory exists
+    match dir.exists() {
+        // If it does, check if it is a directory
+        true => match dir.is_dir() {
+            // If it is a directory, all good
+            true => (),
+            // If it isn't, print an error and return
+            false => {
+                eprintln!("Could not start grade book at provided path. Path is not a directory");
+                return
+            }
+        },
+        // If it doesn't, create it
+        false => fs::create_dir(&dir).expect("Failed to create directory to start grade book")
+    }
+
+    // Now dir points to an actual directory guaranteed
+    // First check if a .grade directory already exists in the dir
+
+    // Get a path that points to the .grade directory
+    let grade_dir = dir.join(".grade");
+    // If it exists and is a directory, exit because the book is already started
+    // If it exists and isn't a directory throw an error
+    if grade_dir.exists() {
+        if grade_dir.is_dir() {
+            eprintln!("Failed to start grade book. {} is not a directory", grade_dir.display())
+        }
+        return
+    }
+    // If it doesn't exist, the grade book needs to be started
+    // TODO
+}
+
+/// Handles the user calling the submit command
+fn handle_submit(args: &ArgMatches) {
+    //TODO
+    println!("Submit called with: ");
+    println!("{:?}", args)
 }
