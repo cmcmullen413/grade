e21bcc55fe29fa7a61eaecc648d0a598d3840128
Caleb McMullen <cmcmullen@gmail.com>
2026-07-13 22:22:36 -0400

    Stub Out test_start.py Methods

--- tests/test_start.py
+++ tests/test_start.py
@@ -1,2 +1,36 @@
-import sh
-import pytest
+BINARY_PATH = "../target/release/grade.exe"
+
+# Tests the start command with a passed in directory that does not exist
+def test_directory_creation():
+    # TODO
+    assert False
+
+# Tests the start command with no provided directory with the working directory being empty
+def test_no_path_provided_empty():
+    # TODO
+    assert False
+
+# Test the start command with no provided directory and the working directory contains a .grade subdirectory
+def test_no_path_provided_non_empty():
+    # TODO
+    assert False
+
+# Tests the start command with a passed in directory that exists, but is empty
+def test_directory_exists_empty():
+    # TODO
+    assert False
+
+# Tests the start command with a passed in directory that exists and contains a .grade subdirectory
+def test_directory_exists_non_empty():
+    # TODO
+    assert False
+
+# Tests the start command with a passed in path that is not a directory
+def test_path_not_directory():
+    # TODO
+    assert False
+
+# Tests the start command with a passed in path where the .grade subpath is not a directory
+def test_grade_path_not_directory():
+    # TODO
+    assert False
